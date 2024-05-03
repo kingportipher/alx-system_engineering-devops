53b51301d375746cc47ad9f39cf7ef85d2f4212d
Victor Abrokwah <kingportipher@outlook.com>
2024-05-03 06:19:04 -0700

    Task 0 redone

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
-file { '/tmp/school':
-  ensure  => 'file',
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => "I love Puppet\n",
+file  {'create-file':
+  ensure  =>  'present',
+  path    =>  '/tmp/school',
+  mode    =>  '0744',
+  owner   =>  'www-data',
+  group   =>  'www-data',
+  content =>  'I love Puppet',
 }
