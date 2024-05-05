f35ef9de7bcde9f934e86ab92e1155b687f556fd
Victor Abrokwah <kingportipher@outlook.com>
2024-05-05 16:15:18 -0700

    Task 1 redone

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,5 @@
-#This file installs flask using puppet
-
-package { 'python3-pip':
-  ensure => 'installed',
-}
-
-exec { 'install_flask':
-  command     => '/usr/bin/pip3 install flask==2.1.0',
-  path        => '/usr/local/bin:/usr/bin:/bin',
-  environment => ['PATH=/usr/local/bin:/usr/bin:/bin'],
-  unless      => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
+# Install flask
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
