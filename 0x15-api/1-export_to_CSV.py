c8320d8870956ba70c537facf7181c6d11722036
Victor Abrokwah <kingportipher@outlook.com>
2024-06-03 18:51:08 -0700

    Task 1 done

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,41 +1,24 @@
 #!/usr/bin/python3
-
+""" Export api to csv"""
 import csv
 import requests
 import sys
 
-if __name__ == "__main__":
-    # Ensure correct number of command-line arguments
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <employee_id>")
-        sys.exit(1)
+if __name__ == '__main__':
+    user = sys.argv[1]
+    url_user = 'https://jsonplaceholder.typicode.com/users/' + user
+    res = requests.get(url_user)
+    """ANYTHING"""
+    user_name = res.json().get('username')
+    task = url_user + '/todos'
+    res = requests.get(task)
+    tasks = res.json()
 
-    # Get the employee ID from the command-line arguments
-    employee_id = sys.argv[1]
-
-    # Base URL for the JSONPlaceholder API
-    url = "https://jsonplaceholder.typicode.com/"
-
-    # Fetch user information from the API and convert the response to a JSON object
-    user_response = requests.get(f"{url}users/{employee_id}")
-    user_data = user_response.json()
-
-    # Extract the username from the user data
-    username = user_data.get("username")
-
-    # Fetch the to-do list items associated with the given user ID
-    # and convert the response to a JSON object
-    todo_response = requests.get(f"{url}todos", params={"userId": employee_id})
-    todos = todo_response.json()
-
-    # Write the to-do list items to a CSV file
-    csv_filename = f"{employee_id}.csv"
-    with open(csv_filename, "w", newline="") as csvfile:
-        writer = csv.writer(csvfile)
-        # Write header row
-        writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-        # Write each task as a row in the CSV file
-        for todo in todos:
-            writer.writerow([employee_id, username, todo["completed"], todo["title"]])
-
-    print(f"Data exported to {csv_filename}")
+    with open('{}.csv'.format(user), 'w') as csvfile:
+        for task in tasks:
+            completed = task.get('completed')
+            """Complete"""
+            title_task = task.get('title')
+            """Done"""
+            csvfile.write('"{}","{}","{}","{}"\n'.format(
+                user, user_name, completed, title_task))
