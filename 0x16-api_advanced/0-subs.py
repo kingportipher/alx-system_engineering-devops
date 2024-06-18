423405915c512cf96a64eb569dfda2abba673223
Victor Abrokwah <kingportipher@outlook.com>
2024-06-18 14:50:25 -0700

    Task 0 done

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Script that queries subscribers on a given Reddit subreddit.
+"""
+
+import requests
+
+def number_of_subscribers(subreddit):
+    """Return the total number of subscribers on a given subreddit."""
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "myCustomUserAgent/0.1"}
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            return data['data'].get('subscribers', 0)
+        elif response.status_code == 404:
+            return 0
+        else:
+            return 0
+    except requests.RequestException:
+        return 0
+
